a7aa8da7aad729393b1c09214aa3cde373bdea73
Emma Lindsay <emma@quartz.co>
2020-03-07 19:19:49 -0800

    Remove absolute imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
 import LoginContextContainer from './contexts/LoginContextContainer.js';
-import Login from './pages/login';
-import Routes from './routes.js';
-import './App.css';
+import Login from 'pages/login';
+import Routes from 'routes.js';
+import 'App.css';
 
 function App() {
   return (
