0b8b239bc7629a51bb3d9d8e9f83776972445dcb
Emma Lindsay <emma@quartz.co>
2020-03-07 19:04:31 -0800

    Troubleshoot with routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
 import LoginContextContainer from 'contexts/LoginContextContainer.js';
 import Login from 'pages/login';
-import Routes from 'routes.js';
+import Routes from './routes.js';
 import './App.css';
 
 function App() {
